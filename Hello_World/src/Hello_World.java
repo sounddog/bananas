54348a89549228332b48dd522a54752aada36219
Patrick <18fongp14@Millshighsch.org>
2017-08-21 08:16:21 -0700

    Patrick Fong

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,13 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Write a description of class Hello_world here.
+		 * 
+		 */
+		
+		        System.out.println("Hello World! From <your name>"); //Display the string
+		    }
+}
