b7320cfdc8749ce916874c401f73d4be4d120194
Patrick <18fongp14@Millshighsch.org>
2017-08-22 09:27:47 -0700

    Patrick Fong!

---
+++ Project2/src/Project2.java
@@ -0,0 +1,11 @@
+//Patrick Fong
+//Second Java  project: comepare print and println
+//august 22,2017
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello -- this is project two");
+		System.out.print("Ilikepickles");
+	}
+
+}
