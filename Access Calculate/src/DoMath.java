0534f77f8641ebc03413372a46349ded183df569
Patrick <18fongp14@Millshighsch.org>
2017-09-05 09:23:03 -0700

    calculate library first rendition

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,14 @@
+/*Patrick Fong
+ * sep 5 2017
+ * runner clas for calc library  methods
+ */
+
+public class DoMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Calculate.avg(50,100));
+	}
+	
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,26 @@
+//Patrick FOng
+// sep 5 2017
+//calc squre of a variable
+//library that will contain various mathematical methods 
+public class Calculate {
+	
+	//method takes int and returns its square
+		public static int square (int operand) {
+		return operand*operand;
+	}
+		//return cube of int
+		public static int cube (int operand) {
+			return operand*operand*operand;
+		}
+		//returns avg of two doubles 
+		public static double avg (double operand, double operand2) {
+			return (operand+operand2)/2;
+		}
+		//returns avg of three doubles
+		public static double avg (double operand, double operand2, double operand3) {
+			return (operand+operand2+operand3)/3;
+		}
+		public static  toDegrees 
+		//final bracket
+}
+
