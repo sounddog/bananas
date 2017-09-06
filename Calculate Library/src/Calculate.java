76637cab212efeb140646ecab82673e9f34898ff
patrick <patrick@pc>
2017-09-06 11:01:58 -0700

    formatting

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,44 +4,44 @@
 //library that will contain various mathematical methods 
 public class Calculate {
 	
-	//method takes int and returns its square
+//method takes int and returns its square
 		public static int square (int x) {
-		return x*x;
+			return x*x;
 	}
-		//return cube of int
+//return cube of int
 		public static int cube (int x) {
 			return x*x*x;
 		}
-		//returns avg of two doubles 
+//returns avg of two doubles 
 		public static double avg (double a, double b) {
 			return (a+b)/2;
 		}
-		//returns avg of three doubles
+//returns avg of three doubles
 		public static double avg (double a, double b, double c) {
 			return (a+b+c)/3;
 		}
-		//converts double from radian to degrees
+//converts double from radian to degrees
 		public static double toDegrees(double x) {
 			return (x*180)/3.14159;
 		}
-		//converts double from degree to radians
+//converts double from degree to radians
 		public static double toRadians( double x) {
 			return (x*3.14159)/180; 
 		}
-		//b^2-4ac
+//b^2-4ac
 		public static double discriminant(double a, double b, double c) {
 			return (b*b)-(4*a*c);
 		}
 		public static String toImproperFrac(int coefficient, int numerator, int denominator) {
 			return ((coefficient*denominator)+numerator)+"/"+denominator; 
 		}		
-		//returns string after foil
-		//must enter in specific order 
-		//string must be entered with its own set of round brackes and quotes in method entered
+//returns string after foil
+//must enter in specific order 
+//string must be entered with its own set of round brackes and quotes in method entered
 		public static String foil(int a, int b, int c, int d, String letter) {
-			//(ax+b)(cx+d)=acx^2+adx+bcx+bd
+//(ax+b)(cx+d)=acx^2+adx+bcx+bd
 			
-		 return (a*b+letter+"^2" )+"+"+((a*d)+(b*c)+letter)+"+"+(b*d);
+			return (a*b+letter+"^2" )+"+"+((a*d)+(b*c)+letter)+"+"+(b*d);
 		}
 		public static String toMixedNum(int numerator, int denominator) {
 			return numerator/denominator+"_"+numerator%denominator+"/2";
@@ -49,29 +49,29 @@ public class Calculate {
 		}
 //sep 6 2017
 		public static boolean isDivisibleBy(int dividend, int divisor) {	
-		if(dividend % divisor == 0) {
-			return true;
-		} else {
-			return false;
-			//LOL
+			if(dividend % divisor == 0) {
+				return true;
+		}	else {
+				return false;
+//LOL
 		}
 		
 	}
-public static double absValue (double x) {
-	if(x>=0) {
+		public static double absValue (double x) {
+			if(x>=0) {
 		return x;
-	} else {
+	} 		else {
 		return -x;
 	}
 }
-public static double max (double x, double y) {
-	if(x>y) {
-		return x; 
-	} else {
-		return y;
+		public static double max (double x, double y) {
+			if(x>y) {
+				return x; 
+			} else {
+				return y;
 	}
 		}
-public static double max (double x, double y, double z) {
+		public static double max (double x, double y, double z) {
 			if (x>y) {
 				return x;
 			} else if(y>z) {
@@ -81,25 +81,21 @@ public static double max (double x, double y, double z) {
 			}
 			
 		}
-public static int min(int x, int y) {
-	if(x>y) {
-		return y;
-	} else {
-		return x;
+		public static int min(int x, int y) {
+			if(x>y) {
+				return y;
+			} else {
+				return x;
 	}
 	
 	}
-public static double round2 (double x) {
-	if((x*1000) % 10 < 5) {
-		return ((x*1000)-((x*1000)%10))/1000;
-	} else {
-		return ((10-((x*1000)%10))+(x*1000))/1000;
+		public static double round2 (double x) {
+			if((x*1000) % 10 < 5) {
+				return ((x*1000)-((x*1000)%10))/1000;
+			} else {
+				return ((10-((x*1000)%10))+(x*1000))/1000;
 	}
 		
 	}
-	//530
-	//686 > 6
-	//987 > 7
-//		24567.7
-//		mult, add, cast
-}
+	
+	}
